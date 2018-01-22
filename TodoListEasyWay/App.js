a0ff46ebd49d55dbaa21614997b447c4a02c388b
“Newton <newton.beck@gmail.com>
2018-01-22 18:34:04 -0200

    Implementa a todo list

--- TodoListEasyWay/App.js
+++ TodoListEasyWay/App.js
@@ -1,13 +1,29 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList } from 'react-native';
 
 export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      tasks: ['Codar meu projeto', 'Estudar RN'],
+      task: ''
+    }
+  }
+
+  _addTask() {
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task)
+    });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <TextInput placeholder='Type your task here' onChangeText={(text) => this.setState({task: text})} />
+        <Button title='Add' onPress={this._addTask.bind(this)} />
+        <FlatList
+        data={this.state.tasks}
+        renderItem={({item}) => <Text>{item}</Text>} />
       </View>
     );
   }
@@ -16,6 +32,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 20,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
